a47bf8b10f308071350312393020a97c460112f9
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2024-03-04 16:34:34 -0600

    refactor: ♻️ Remove duplicate config that's in core settings.

--- config/nautobot_config.py
+++ config/nautobot_config.py
@@ -1,47 +1,11 @@
 """Nautobot development configuration file."""
+
 # pylint: disable=invalid-envvar-default
 import os
 import sys
 
 from nautobot.core.settings import *  # noqa: F403  # pylint: disable=wildcard-import,unused-wildcard-import
 from nautobot.core.settings_funcs import parse_redis_connection, is_truthy
-
-
-#
-# Misc. settings
-#
-
-ALLOWED_HOSTS = os.getenv("NAUTOBOT_ALLOWED_HOSTS", "").split(" ")
-SECRET_KEY = os.getenv("NAUTOBOT_SECRET_KEY", "")
-
-
-nautobot_db_engine = os.getenv("NAUTOBOT_DB_ENGINE", "django.db.backends.postgresql")
-default_db_settings = {
-    "django.db.backends.postgresql": {
-        "NAUTOBOT_DB_PORT": "5432",
-    },
-    "django.db.backends.mysql": {
-        "NAUTOBOT_DB_PORT": "3306",
-    },
-}
-DATABASES = {
-    "default": {
-        "NAME": os.getenv("NAUTOBOT_DB_NAME", "nautobot"),  # Database name
-        "USER": os.getenv("NAUTOBOT_DB_USER", ""),  # Database username
-        "PASSWORD": os.getenv("NAUTOBOT_DB_PASSWORD", ""),  # Database password
-        "HOST": os.getenv("NAUTOBOT_DB_HOST", "localhost"),  # Database server
-        "PORT": os.getenv(
-            "NAUTOBOT_DB_PORT",
-            default_db_settings[nautobot_db_engine]["NAUTOBOT_DB_PORT"],
-        ),  # Database port, default to postgres
-        "CONN_MAX_AGE": int(os.getenv("NAUTOBOT_DB_TIMEOUT", 300)),  # Database timeout
-        "ENGINE": nautobot_db_engine,
-    }
-}
-
-# Ensure proper Unicode handling for MySQL
-if DATABASES["default"]["ENGINE"] == "django.db.backends.mysql":
-    DATABASES["default"]["OPTIONS"] = {"charset": "utf8mb4"}
 
 #
 # Debug
@@ -56,42 +20,6 @@ TESTING = len(sys.argv) > 1 and sys.argv[1] == "test"
 #
 
 LOG_LEVEL = "DEBUG" if DEBUG else "INFO"
-
-# Verbose logging during normal development operation, but quiet logging during unit test execution
-if not TESTING:
-    LOGGING = {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "formatters": {
-            "normal": {
-                "format": "%(asctime)s.%(msecs)03d %(levelname)-7s %(name)s :\n  %(message)s",
-                "datefmt": "%H:%M:%S",
-            },
-            "verbose": {
-                "format": "%(asctime)s.%(msecs)03d %(levelname)-7s %(name)-20s %(filename)-15s %(funcName)30s() :\n  %(message)s",
-                "datefmt": "%H:%M:%S",
-            },
-        },
-        "handlers": {
-            "normal_console": {
-                "level": "INFO",
-                "class": "logging.StreamHandler",
-                "formatter": "normal",
-            },
-            "verbose_console": {
-                "level": "DEBUG",
-                "class": "logging.StreamHandler",
-                "formatter": "verbose",
-            },
-        },
-        "loggers": {
-            "django": {"handlers": ["normal_console"], "level": "INFO"},
-            "nautobot": {
-                "handlers": ["verbose_console" if DEBUG else "normal_console"],
-                "level": LOG_LEVEL,
-            },
-        },
-    }
 
 #
 # Redis
